e4d4f2f6504fd0dd723e033dbf8d9465c7530ea7
keshavanandsingh <keshavanand22@gmail.com>
2020-10-07 01:26:07 +0530

    added

--- src/MaximumTest/MaximumTestTest.java
+++ src/MaximumTest/MaximumTestTest.java
@@ -6,7 +6,11 @@ import org.junit.Test;
 class MaximumTestTest {
 	@Test
 	public void testMaximumNumber() {
-		assertEquals((Integer) 4, MaximumTest.maxNumber(2, 4, -9));
+		// TC2.1
+		assertEquals((Float) 4.5f, MaximumTest.maxNumber(4.5f, 2.6f, -9.8f));
+		// TC2.2
+		assertEquals((Float) 4.5f, MaximumTest.maxNumber(2.6f, 4.5f, -9.8f));
+		// TC2.3
+		assertEquals((Float) 4.5f, MaximumTest.maxNumber(-9.8f, 2.6f, 4.5f));
 	}
-
 }
