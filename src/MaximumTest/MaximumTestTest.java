cdb86844b4205323e8d3b112a20ba51ab14ae5fa
keshavanandsingh <keshavanand22@gmail.com>
2020-10-07 01:20:41 +0530

    added

--- src/MaximumTest/MaximumTestTest.java
+++ src/MaximumTest/MaximumTestTest.java
@@ -1,12 +1,12 @@
 package MaximumTest;
+
 import static org.junit.Assert.*;
 import org.junit.Test;
+
 class MaximumTestTest {
 	@Test
 	public void testMaximumNumber() {
-		assertEquals((Integer) 4, MaximumTest.maxNumber(4, 2, -9));
+		assertEquals((Integer) 4, MaximumTest.maxNumber(2, 4, -9));
 	}
 
-	
-	}
-
+}
