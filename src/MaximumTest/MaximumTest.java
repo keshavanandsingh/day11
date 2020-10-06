0a917ca619e5a4281ecf044729cae87d357b7a78
keshavanandsingh <keshavanand22@gmail.com>
2020-10-07 01:15:50 +0530

    added

--- src/MaximumTest/MaximumTest.java
+++ src/MaximumTest/MaximumTest.java
@@ -1,11 +1,25 @@
 package MaximumTest;
 
-public class MaximumTest {
-	@Test
-	public void testMaximum() {
-		assertEquals((Integer) 6, MaximumTest.maximum(4, 6, -9));
-		assertEquals((Float) 4.5f, MaximumTest.maximum(4.5f, 2.6f, -9.8f));
-		assertEquals("Peach", MaximumTest.maximum("Banana", "Apple", "Peach"));
+public class MaximumTest<T extends Comparable<T>> {
+	T a, b, c;
+
+	public MaximumTest(T a, T b, T c) {
+		super();
+		this.a = a;
+		this.b = b;
+		this.c = c;
 	}
 
+	public T maximum() {
+		return MaximumTest.maximum(a, b, c);
+	}
+
+	public static <T extends Comparable<T>> T maximum(T a, T b, T c) {
+		T max = a;
+		if (b.compareTo(max) > 0)
+			max = b;
+		if (c.compareTo(max) > 0)
+			max = c;
+		return max;
+	}
 }
--- src/MaximumTest/MaximumTestTest.java
+++ src/MaximumTest/MaximumTestTest.java
@@ -1,17 +1,27 @@
 package MaximumTest;
 
 import static org.junit.Assert.*;
-import org.junit.Before;
 import org.junit.Test;
 
 class MaximumTestTest {
+	MaximumTest maximumTest;
+
+	@Test
+	public void testMaximumInteger() {
+		maximumTest = new MaximumTest<>(2, 8, -12);
+		assertEquals(8, maximumTest.maximum());
+	}
+
+	@Test
+	public void testMaximumFloat() {
+		maximumTest = new MaximumTest<>(4.5f, 2.6f, -9.8f);
+		assertEquals(4.5f, maximumTest.maximum());
+	}
+
 	@Test
 	public void testMaxString() {
-		// TC3.1
-		assertEquals("Peach", MaximumTest.maxString("Peach", "Apple", "Banana"));
-		// TC3.2
-		assertEquals("Peach", MaximumTest.maxString("Apple", "Peach", "Banana"));
-		// TC3.3
-		assertEquals("Peach", MaximumTest.maxString("Banana", "Apple", "Peach"));
+		maximumTest = new MaximumTest<>("Banana", "Apple", "Peach");
+		assertEquals("Peach", maximumTest.maximum());
 	}
+
 }
