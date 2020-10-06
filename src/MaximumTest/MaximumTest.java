034c8fb8cca5bfae67d9812d40a08848da99ad4c
keshavanandsingh <keshavanand22@gmail.com>
2020-10-07 01:34:28 +0530

    added

--- src/MaximumTest/MaximumTest.java
+++ src/MaximumTest/MaximumTest.java
@@ -1,8 +1,19 @@
 package MaximumTest;
 
 public class MaximumTest {
+	// UC1
 	public static Integer maxNumber(Integer a, Integer b, Integer c) {
 		Integer max = a;
+		if (b.compareTo(max) > 0)
+			max = b;
+		if (c.compareTo(max) > 0)
+			max = c;
+		return max;
+	}
+
+	// UC2
+	public static Float maxNumber(Float a, Float b, Float c) {
+		Float max = a;
 		if (b.compareTo(max) > 0)
 			max = b;
 		if (c.compareTo(max) > 0)
