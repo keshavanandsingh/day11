beef960eff1b886fc867519916e763819f48ffad
keshavanandsingh <keshavanand22@gmail.com>
2020-10-07 01:44:22 +0530

    added

--- src/MaximumTest/MaximumTestTest.java
+++ src/MaximumTest/MaximumTestTest.java
@@ -1,7 +1,7 @@
 package MaximumTest;
 
 import static org.junit.Assert.*;
-import org.junit.Test
+import org.junit.Test;
 
 class MaximumTestTest {
 	MaximumTest maximumTest;
@@ -29,7 +29,8 @@ class MaximumTestTest {
 
 	@Test
 	public void testMaximumWithNoElement() {
-		// taking 5 parameters in
+		// taking no parameter
+		// should return null instead of throwing an error
 		maximumTest = new MaximumTest<>();
 		assertEquals(null, maximumTest.maximum());
 	}
