9814616b6161d0497981737b339dbd4ac1e98a58
keshavanandsingh <keshavanand22@gmail.com>
2020-10-07 01:18:04 +0530

    added

--- src/MaximumTest/MaximumTestTest.java
+++ src/MaximumTest/MaximumTestTest.java
@@ -1,27 +1,12 @@
 package MaximumTest;
-
 import static org.junit.Assert.*;
 import org.junit.Test;
-
 class MaximumTestTest {
-	MaximumTest maximumTest;
-
 	@Test
-	public void testMaximumInteger() {
-		maximumTest = new MaximumTest<>(2, 8, -12);
-		assertEquals(8, maximumTest.maximum());
+	public void testMaximumNumber() {
+		assertEquals((Integer) 4, MaximumTest.maxNumber(4, 2, -9));
 	}
 
-	@Test
-	public void testMaximumFloat() {
-		maximumTest = new MaximumTest<>(4.5f, 2.6f, -9.8f);
-		assertEquals(4.5f, maximumTest.maximum());
+	
 	}
 
-	@Test
-	public void testMaxString() {
-		maximumTest = new MaximumTest<>("Banana", "Apple", "Peach");
-		assertEquals("Peach", maximumTest.maximum());
-	}
-
-}
