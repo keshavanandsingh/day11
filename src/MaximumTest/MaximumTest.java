ffc998cf017dc393104f4268bcfb454fa7725829
keshavanandsingh <keshavanand22@gmail.com>
2020-10-07 01:22:59 +0530

    added

--- src/MaximumTest/MaximumTest.java
+++ src/MaximumTest/MaximumTest.java
@@ -1,25 +1,13 @@
 package MaximumTest;
 
-public class MaximumTest<T extends Comparable<T>> {
-	T a, b, c;
-
-	public MaximumTest(T a, T b, T c) {
-		super();
-		this.a = a;
-		this.b = b;
-		this.c = c;
-	}
-
-	public T maximum() {
-		return MaximumTest.maximum(a, b, c);
-	}
-
-	public static <T extends Comparable<T>> T maximum(T a, T b, T c) {
-		T max = a;
+public class MaximumTest {
+	public static Integer maxNumber(Integer a, Integer b, Integer c) {
+		Integer max = a;
 		if (b.compareTo(max) > 0)
 			max = b;
 		if (c.compareTo(max) > 0)
 			max = c;
 		return max;
 	}
+
 }
